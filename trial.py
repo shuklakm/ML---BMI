09b43986ab21dd98db43042cb132bb54c93100cf
shuklakm <118303126+shuklakm@users.noreply.github.com>
2023-05-19 02:43:54 -0500

    Create trial.py

---
+++ trial.py
@@ -0,0 +1,10 @@
+def main():
+    st.title("Real-time BMI Detection")
+
+    # File Uploader
+    st.subheader("File Uploader")
+    uploaded_file = st.file_uploader("Please choose a file")
+        
+
+if __name__ == '__main__':
+    main()
